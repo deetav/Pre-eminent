02debbfd9cb5cafbf2a671a6b302f67d6ee6273c
Deepak Srivastava <deetav001@gmail.com>
2021-07-28 23:24:55 +0530

    deployed firebase

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [todos, setTodos] = useState(['abs','sba']);
   const [input, setInput] = useState("");
   useEffect(() => {
-    db.collection('todos').onSnapshot((snapshot) => {
+    db.collection('todos').orderBy('timestamp','desc').onSnapshot((snapshot) => {
       setTodos(snapshot.docs.map((doc) => doc.data().todo));
     });
   }, []);
